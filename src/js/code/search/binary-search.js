eeaac798e67d7cd86143bc84e7fb1025bb65fc3c
zbxiang <1144653975@qq.com>
2022-03-07 22:09:49 +0800

    插入数据

---
+++ src/js/code/search/binary-search.js
@@ -0,0 +1,33 @@
+import { Compare, defaultCompare } from '../../util'
+import { Node } from './module/node'
+
+export default class BinarySearchTree {
+    constructor(compareFn = defaultCompare) {
+        this.compareFn = compareFn
+        this.root = null
+    }
+
+    insert(key) {
+        if (this.root == null) {
+            this.root = new Node(key)
+        } else {
+            this.insertNode(this.root, key)
+        }
+    }
+
+    insertNode(node, key) {
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if (node.left == null) {
+                node.left = new Node(key)
+            } else {
+                this.insertNode(node.left, key)
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new Node(key)
+            } else {
+                this.insertNode(node.right, key)
+            }
+        }
+    }
+}
